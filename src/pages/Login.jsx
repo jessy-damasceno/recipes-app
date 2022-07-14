6c4c19bbeb920c557e24b822a9bb24aadde23eb9
Jessy Damasceno <jessy_bas@hotmail.com>
2022-07-14 17:16:04 -0300

    ADD: login css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
+import '../styles/Login.css';
 
 function Login() {
   const history = useHistory();
@@ -20,23 +22,30 @@ function Login() {
 
   return (
     <div className="main-container">
-      <form>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          data-testid="email-input"
-          value={ email }
-          onChange={ (e) => setEmail(e.target.value) }
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          data-testid="password-input"
-          value={ password }
-          onChange={ (e) => setPassword(e.target.value) }
-        />
+      <section className="login">
+        <div className="avatar">
+          <img src={ profileIcon } alt="Profile Icon" />
+        </div>
+        <form className="login-inputs">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            data-testid="email-input"
+            placeholder="Type your email"
+            value={ email }
+            onChange={ (e) => setEmail(e.target.value) }
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            data-testid="password-input"
+            placeholder="Type your password"
+            value={ password }
+            onChange={ (e) => setPassword(e.target.value) }
+          />
+        </form>
         <button
           type="button"
           data-testid="login-submit-btn"
@@ -45,7 +54,7 @@ function Login() {
         >
           Enter
         </button>
-      </form>
+      </section>
     </div>
   );
 }
