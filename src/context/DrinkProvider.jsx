84092a1eeeb7f70f487d344863f49183c7b69d6a
Jessy Damasceno <jessy_bas@hotmail.com>
2022-07-18 18:30:16 -0300

    UPDATE: DrinkProvider

--- src/context/DrinkProvider.jsx
+++ src/context/DrinkProvider.jsx
@@ -1,23 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { drinkContext } from './context';
-import { fetchDrinks } from '../services/fetchDrinks';
+import { fetchDrinkByCategories, fetchDrinks } from '../services/fetchDrinks';
 
 function DrinkProvider({ children }) {
+  const [drinksData, setDrinksData] = useState([]);
   const [drinks, setDrinks] = useState([]);
+  const [atualDrinkCategory, setAtualDrinkCategory] = useState([]);
 
   useEffect(() => {
     const fetchAPI = async () => {
       const response = await fetchDrinks();
-      setDrinks(response);
+      setDrinksData(response);
     };
 
     fetchAPI();
   }, []);
 
+  useEffect(() => {
+    const fetchAPI = () => {
+      setDrinks(drinksData);
+    };
+
+    fetchAPI();
+  }, [drinksData]);
+
+  const setDrinksByCategory = async (category) => {
+    if (category === atualDrinkCategory || category === 'All') {
+      setDrinks(foodData);
+      setAtualDrinkCategory('All');
+    } else {
+      const response = await fetchDrinkByCategories(category);
+      setAtualDrinkCategory(category);
+      setDrinks(response);
+    }
+  };
+
   const contextValue = {
+    // states
     drinks,
+    // funcs
     setDrinks,
+    setDrinksByCategory,
   };
 
   return (
