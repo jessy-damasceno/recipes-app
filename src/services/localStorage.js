9b7bbf567e8b82252fb5e88e52da563872cf7f3b
Jessy Damasceno <jessy_bas@hotmail.com>
2022-07-20 00:45:06 -0300

    FIX coisas

--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -1,5 +1,6 @@
 export const KEY_USER = 'user';
 export const DONE_RECIPES = 'doneRecipes';
+export const IN_PROGRESS_RECIPES = 'inProgressRecipes';
 
 export function getEmail() {
   const response = localStorage.getItem(KEY_USER);
@@ -27,4 +28,32 @@ export const addDoneRecipe = (recipe) => {
   } else {
     localStorage.setItem(DONE_RECIPES, JSON.stringify([recipe]));
   }
+};
+
+export const getRecipesInProgress = () => {
+  const recipesInProgress = localStorage.getItem(IN_PROGRESS_RECIPES);
+
+  if (recipesInProgress) {
+    return JSON.parse(recipesInProgress);
+  }
+  return {
+    cocktails: {},
+    meals: {},
+  };
+};
+
+export const verifyMealIsInProgress = (id) => {
+  const inProgressRecipes = getRecipesInProgress();
+
+  if (Object.keys(inProgressRecipes.meals).length > 1) {
+    return Object.keys(JSON.parse(inProgressRecipes.meals)).includes(id);
+  }
+  return false;
+};
+
+export const addMealInProgress = (key, value) => {
+  const inProgressRecipes = getRecipesInProgress();
+  inProgressRecipes.meals[key] = value;
+
+  localStorage.setItem(IN_PROGRESS_RECIPES, JSON.stringify(inProgressRecipes));
 };
