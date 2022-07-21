f73ec49120f99546d966b2bdb8f522651f38dafd
Joao Pedro <ujoaoo7001@gmail.com>
2022-07-21 16:52:51 -0300

    Refatorando

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import FavoriteAllRecipes from '../components/FavoriteAllRecipes copy';
+import FavoriteAllRecipes from '../components/FavoriteAllRecipes';
 // import FavoriteDrinksRecipes from '../components/FavoriteDrinksRecipes';
 // import FavoriteFoodRecipes from '../components/FavoriteFoodRecipes copy';
 import Header from '../components/Header';
@@ -7,26 +7,6 @@ import Header from '../components/Header';
 const FavoriteRecipes = () => (
   <main>
     <Header title="Favorite Recipes" />
-    <div>
-      <button
-        type="button"
-        data-testid="filter-by-all-btn"
-      >
-        All
-      </button>
-      <button
-        type="button"
-        data-testid="filter-by-food-btn"
-      >
-        Food
-      </button>
-      <button
-        type="button"
-        data-testid="filter-by-drink-btn"
-      >
-        Drinks
-      </button>
-    </div>
     <FavoriteAllRecipes />
   </main>
 );
