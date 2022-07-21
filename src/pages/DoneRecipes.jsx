5d59739b0ce565edd5972ec958ce7a82c6a110f5
Herdina <gherdina7@gmail.com>
2022-07-21 17:35:33 -0300

    FIX: finish recipe button

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
 import Header from '../components/Header';
 
-const DoneRecipes = () => (
-  <main>
-    <Header title="Done Recipes" />
-  </main>
-);
+function DoneRecipes() {
+  return (
+    <main>
+      <Header title="Done Recipes" />
+      <section>
+        <div>
+          <button
+            type="button"
+            data-testid="filter-by-all-btn"
+          >
+            All
+          </button>
+          <button
+            type="button"
+            data-testid="filter-by-food-btn"
+          >
+            Food
+          </button>
+          <button
+            type="button"
+            data-testid="filter-by-drink-btn"
+          >
+            Drinks
+          </button>
+        </div>
+        {/* <h3 data-testid={ `${index}-horizontal-name` }> </h3>
+        <h3 data-testid={ `${index}-horizontal-top-text` }> </h3>
+        <h3 data-testid={ `${index}-horizontal-done-date` }> </h3>
+        <img
+          src={ e }
+          alt={ e }
+          data-testid={ `${index}-horizontal-image` }
+        />
+        <h3 data-testid={ `${index}-${tags}-horizontal-tag` }> </h3>
+        <img
+          src={ e }
+          alt={ e }
+          data-testid={ `${index}-horizontal-share-btn` }
+        /> */}
+      </section>
+    </main>
+  );
+}
 
 export default DoneRecipes;
