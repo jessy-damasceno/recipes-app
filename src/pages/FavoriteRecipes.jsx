94531bae7b3cc1e22693211949700fdbd92cc70f
Herdina <gherdina7@gmail.com>
2022-07-21 20:22:37 -0300

    ADD: tela de favoritos. Faltam apenas testes

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import PropTypes from 'prop-types'; /
 import { Link } from 'react-router-dom';
 import clipboardCopy from 'clipboard-copy';
-import { getFavoriteRecipes } from '../services/localStorage';
+import { getFavoriteRecipes, removeFavoriteRecipe } from '../services/localStorage';
 import shareIcon from '../images/shareIcon.svg';
 import blackHeart from '../images/blackHeartIcon.svg';
 import '../styles/FavoriteRecipes.css';
-// import whiteHeart from '../images/whiteHeartIcon.svg';
 
 const FavoriteRecipes = () => {
-  const favoriteAll = getFavoriteRecipes();
+  const [favorites, setFavorite] = useState([]);
   const [isClicked, setIsClicked] = useState(false);
+
+  useEffect(() => {
+    const favoriteRecipes = getFavoriteRecipes();
+    setFavorite(favoriteRecipes);
+  }, []);
 
   const shareFunction = (type, id) => {
     setIsClicked(true);
@@ -20,134 +24,94 @@ const FavoriteRecipes = () => {
     }, +'2000');
   };
 
+  const handleClick = (type) => {
+    const newFavorites = getFavoriteRecipes();
+    setFavorite(newFavorites.filter((favorite) => favorite.type === type));
+  };
+
+  const removeFavorite = (id) => {
+    setFavorite((prev) => prev.filter((fav) => fav.id !== id));
+    removeFavoriteRecipe(id);
+  };
+
   return (
     <div>
       <div>
         <button
           type="button"
           data-testid="filter-by-all-btn"
+          onClick={ () => setFavorite(getFavoriteRecipes()) }
         >
           All
         </button>
         <button
           type="button"
           data-testid="filter-by-food-btn"
+          onClick={ () => handleClick('food') }
         >
           Food
         </button>
         <button
           type="button"
           data-testid="filter-by-drink-btn"
+          onClick={ () => handleClick('drink') }
         >
           Drinks
         </button>
       </div>
-      {favoriteAll.map((
-        { alcoholicOrNot, name, image, nationality, category, type, id }, i,
-      ) => {
-        console.log(type);
-        if (type === 'drink') {
-          return (
-            <div
-              className="favorite-recipes-container"
-              key={ i }
+      {Boolean(favorites?.length) && favorites.map((e, index) => (
+        <div
+          className="favorite-recipes-container"
+          key={ index }
+        >
+          <Link to={ `/${e.type}s/${e.id}` }>
+            <h4
+              data-testid={ `${index}-horizontal-top-text` }
             >
-              <p
-                data-testid={ `${i}-horizontal-top-text` }
-              >
-                { alcoholicOrNot }
-              </p>
-              <Link to={ `/drinks/${id}` }>
-                <img
-                  src={ image }
-                  data-testid={ `${i}-horizontal-image` }
-                  className="favorite-recipe-image"
-                  alt=""
-                />
-                <h1
-                  data-testid={ `${i}-horizontal-name` }
-                >
-                  { name }
-                </h1>
-              </Link>
-              <button
-                type="button"
-                onClick={ shareFunction }
-              >
-                <img
-                  src={ shareIcon }
-                  alt="share icon"
-                  data-testid={ `${i}-horizontal-share-btn` }
-                />
-              </button>
-              <button
-                type="button"
-                // onClick={ removeFavoriteFunction }
-              >
-                <img
-                  src={ blackHeart }
-                  data-testid={ `${i}-horizontal-favorite-btn` }
-                  alt="favorite icon"
-                />
-              </button>
-              {isClicked && <span className="copied_span">Link copied!</span>}
-            </div>
-          );
-        }
-        return (
-          <div
-            className="favorite-recipes-container"
-            key={ i }
+              {e.type === 'food'
+                ? `${e.nationality} - ${e.category}`
+                : e.alcoholicOrNot}
+            </h4>
+            <img
+              src={ e.image }
+              data-testid={ `${index}-horizontal-image` }
+              className="favorite-recipe-image"
+              alt=""
+            />
+            <h2
+              data-testid={ `${index}-horizontal-name` }
+            >
+              { e.name }
+            </h2>
+          </Link>
+          <button
+            type="button"
+            onClick={ () => shareFunction(e.type, e.id) }
           >
-            <p
-              data-testid={ `${i}-horizontal-top-text` }
-            >
-              { nationality }
-              {' - '}
-              { category }
-            </p>
-            <Link to={ `/foods/${id}` }>
-              <img
-                src={ image }
-                data-testid={ `${i}-horizontal-image` }
-                className="favorite-recipe-image"
-                alt=""
-              />
-              <h1
-                data-testid={ `${i}-horizontal-name` }
-              >
-                {name}
-              </h1>
-            </Link>
-            <button
-              type="button"
-              onClick={ () => shareFunction(type, id) }
-            >
-              <img
-                src={ shareIcon }
-                alt="share icon"
-                data-testid={ `${i}-horizontal-share-btn` }
-              />
-            </button>
-            <button
-              type="button"
-              // onClick={}
-            >
-              <img
-                src={ blackHeart }
-                data-testid={ `${i}-horizontal-favorite-btn` }
-                alt="favorite icon"
-              />
-            </button>
-            {isClicked && <span className="copied_span">Link copied!</span>}
-          </div>
-        );
-      })}
+            <img
+              src={ shareIcon }
+              alt="share icon"
+              data-testid={ `${index}-horizontal-share-btn` }
+            />
+          </button>
+          <button
+            type="button"
+            onClick={ () => removeFavorite(e.id) }
+          >
+            <img
+              src={ blackHeart }
+              data-testid={ `${index}-horizontal-favorite-btn` }
+              alt="favorite icon"
+            />
+          </button>
+          {isClicked && <span className="copied_span">Link copied!</span>}
+        </div>
+      ))}
     </div>
   );
 };
 
-// FavoriteDrinksRecipes.propTypes = {
+// FavoriteRecipes.propTypes = {
 //   // id: PropTypes.string.isRequired,
 // };
 
