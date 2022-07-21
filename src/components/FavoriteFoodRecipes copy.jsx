24dec96f40707ef2273e98ad3d258bbba17863fb
Joao Pedro <ujoaoo7001@gmail.com>
2022-07-21 08:11:37 -0300

    ADD: FavoriteRecipes

---
+++ src/components/FavoriteFoodRecipes copy.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const FavoriteFoodRecipes = () => (
+  <div>
+    <h1>Food</h1>
+  </div>
+);
+
+// FavoriteFoodRecipes.propTypes = {};
+
+export default FavoriteFoodRecipes;
